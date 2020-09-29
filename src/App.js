c8dfe41c26a52415f440bbdff0017eb17e441bdb
Rafal Borowski <rafall.borowski@gmail.com>
2020-09-29 21:00:32 +0200

    Login Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
+import Login from './components/Login';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -9,14 +10,17 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
           <Route exact path="/">
+            <Header />
             <Home />
           </Route>
           <Route exact path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+          <Route exact path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    // login firebase
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    //register firebase
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          alt="logo"
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form action="">
+          <h5>E-mail</h5>
+          <input type="text" value={email} onChange={handleEmail} />
+          <h5>Password</h5>
+          <input type="password" value={password} onChange={handlePassword} />
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your amazon account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
